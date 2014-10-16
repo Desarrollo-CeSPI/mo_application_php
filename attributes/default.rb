6b847345797d5de3fbfb3564144f4ba9340e1a3a
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-10-16 15:27:13 -0300

    Update metadata

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 default['nginx']['default_site_enabled'] = false
+default["php_fpm"]["update_system"] = false
+default["php_fpm"]["upgrade_system"] = false
 default['cespi_application_php']['command'] = "php -dmemory_limit=512M"
