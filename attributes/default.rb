4805dc9668788c3d0e7c166ba6763bb22d3cf11f
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-10-27 09:54:11 -0300

    Raises default['nginx']['types_hash_bucket_size'] to 128

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['nginx']['default_site_enabled'] = false
+default['nginx']['types_hash_bucket_size'] = 128
 default["php_fpm"]["update_system"] = false
 default["php_fpm"]["upgrade_system"] = false
 default['cespi_application_php']['command'] = "php -dmemory_limit=512M"
