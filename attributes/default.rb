e26f7f537474880116257e75ebdf8b0933e041eb
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-10-27 10:16:08 -0300

    Bad attribute overloading for nginx
    It is server_names_hash_bucket_size not types_hash_bucket_size

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['nginx']['default_site_enabled'] = false
-default['nginx']['types_hash_bucket_size'] = 128
+default['nginx']['server_names_hash_bucket_size'] = 128
 default["php_fpm"]["update_system"] = false
 default["php_fpm"]["upgrade_system"] = false
 default['cespi_application_php']['command'] = "php -dmemory_limit=512M"
