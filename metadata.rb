5fa13591fc8b08fa282eee2985d01162ac5f5e13
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-10-31 15:44:20 -0300

    Upgrade php5-fpm dependency

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email 'car@cespi.unlp.edu.ar lditommaso@cespi.unlp.edu.ar'
 license          'All rights reserved'
 description      'Installs/Configures cespi_application_php'
 long_description 'Installs/Configures cespi_application_php'
-version          '0.1.2'
+version          '0.1.3'
 
 depends         'cespi_application',  '~> 0.1.0'
 depends         'hostsfile',          '~> 2.4.5'
-depends         'php5-fpm',           "~> 0.2.1"
+depends         'php5-fpm',           "~> 0.3.1"
 depends         'nginx',              "~> 2.7.4"
 depends         'nginx_conf',         "~> 0.2.4"
 depends         'wkhtmltox',          "~> 0.1.0"
