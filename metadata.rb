85cc37b7f046114d3cdcb6647b96cfb5aedec822
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-14 08:04:50 -0300

    Restrict ubuntu 12.04 or less

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email 'chrodriguez@gmail.com leandro.ditommaso@mikroways.net'
 license          'All rights reserved'
 description      'Installs/Configures mo_application_php'
 long_description 'Installs/Configures mo_application_php'
-version          '0.1.14'
+version          '0.1.15'
 
 depends         'mo_application',     "~> 0.1.1"
 depends         'php5-fpm',           "~> 0.3.1"
 depends         'wkhtmltox',          "~> 0.1.0"
 depends         'chef-msttcorefonts', "~> 0.9.0"
 depends         'sudo',               "~> 2.7.1"
+
+supports        "ubuntu", "<= 12.04"
