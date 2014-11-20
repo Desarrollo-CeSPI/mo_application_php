8c2c253106e0e4fb5ea4d8be8c2c0a7cf864cd84
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-20 18:53:25 -0300

    Install php5-imagick

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,5 +4,5 @@ include_recipe 'php5-fpm::configure_fpm'
 include_recipe 'wkhtmltox::default'
 include_recipe 'mo_application_php::_fix_php_fpm_pools'
 
-%w(git php5-cli php5-mysqlnd php5-xsl php-apc).each { |p| package p }
+%w(git php5-cli php5-mysqlnd php5-xsl php5-imagick php-apc).each { |p| package p }
 
