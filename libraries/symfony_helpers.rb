518e221bd9d9411cd77e12523b6cefebb27692c7
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-12-03 13:09:28 -0300

    Avoid resource to be called more than once

--- libraries/symfony_helpers.rb
+++ libraries/symfony_helpers.rb
@@ -1,14 +1,18 @@
 def mo_symfony_clear_cache
-  ruby_block "mo_symfony_clear_cache_#{new_resource.name}" do
-    block do
-      cmd = Mixlib::ShellOut.new("#{php_command} symfony cache:clear",
-                                 :user => new_resource.user,
-                                 :env => nil,
-                                 :cwd => ::File.join(new_resource.path, new_resource.relative_path, 'current'))
-      cmd.run_command
+  cc_resource = "mo_symfony_clear_cache_#{new_resource.name}"
+  run_context.resource_collection.find("ruby_block[#{cc_resource}]")
+  rescue Chef::Exceptions::ResourceNotFound
+    # resource_collection#find raises an exception. In that case we define this resource for the first time
+    ruby_block cc_resource do
+      block do
+        cmd = Mixlib::ShellOut.new("#{php_command} symfony cache:clear",
+                                   :user => new_resource.user,
+                                   :env => nil,
+                                   :cwd => ::File.join(new_resource.path, 'current'))
+        cmd.run_command
+      end
+      action :nothing
     end
-    action :nothing
-  end
 end
 
 def symfony_application_template(name, &block)
