4534952535c39297009727b2a722fbada314faa7
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-07-06 12:50:05 -0300

    Symfony apps always must be migrated

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrodriguez@gmail.com leandro.ditommaso@mikroways.net'
 license          'All rights reserved'
 description      'Installs/Configures mo_application_php'
 long_description 'Installs/Configures mo_application_php'
-version          '1.1.9'
+version          '1.1.10'
 
 depends         'mo_application',         "~> 1.1.0"
 depends         'mo_monitoring_client',   "~> 1.0.1"
